03fb06c59c3b57ad89d5478876d3b61d06209621
Ciprian Carausu <carausuciprian@gmail.com>
2020-11-06 14:42:32 +0200

    User model addeD

--- src/main/java/cyker/app/webservices/ui/controller/UserController.java
+++ src/main/java/cyker/app/webservices/ui/controller/UserController.java
@@ -1,5 +1,6 @@
-package cyker.app.webservices.controller;
+package cyker.app.webservices.ui.controller;
 
+import cyker.app.webservices.ui.model.request.UserDetailsRequestModel;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +13,7 @@ public class UserController {
     }
 
     @PostMapping
-    public String createUser(){
+    public String createUser(@RequestBody UserDetailsRequestModel userDetails){
         return "create user was called";
     }
 

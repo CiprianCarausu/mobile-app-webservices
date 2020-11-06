1225fdaf69561923abc095f82825e91db90e6fa8
Ciprian Carausu <carausuciprian@gmail.com>
2020-11-06 16:01:05 +0200

    UserDto updated

--- src/main/java/cyker/app/webservices/shared/dto/UserDto.java
+++ src/main/java/cyker/app/webservices/shared/dto/UserDto.java
@@ -3,7 +3,8 @@ package cyker.app.webservices.shared.dto;
 import java.io.Serializable;
 
 public class UserDto implements Serializable {
-    private static final long serialVersionUID = 4865903039190150223L;
+
+    private static final long serialVersionUID = -7515252376054854416L;
     private long id;
     private String firstName;
     private String lastName;
@@ -13,5 +14,67 @@ public class UserDto implements Serializable {
     private String emailVerificationToken;
     private Boolean emailVerificationStatus;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getEmailVerificationToken() {
+        return emailVerificationToken;
+    }
+
+    public void setEmailVerificationToken(String emailVerificationToken) {
+        this.emailVerificationToken = emailVerificationToken;
+    }
+
+    public Boolean getEmailVerificationStatus() {
+        return emailVerificationStatus;
+    }
+
+    public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+        this.emailVerificationStatus = emailVerificationStatus;
+    }
 }

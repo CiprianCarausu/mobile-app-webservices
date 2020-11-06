85cd38eb73153a095c790e4b64a2f242b5523128
Ciprian Carausu <carausuciprian@gmail.com>
2020-11-06 16:35:55 +0200

    impl modified

--- src/main/java/cyker/app/webservices/service/Impl/UserServiceImpl.java
+++ src/main/java/cyker/app/webservices/service/Impl/UserServiceImpl.java
@@ -1,13 +1,31 @@
 package cyker.app.webservices.service.Impl;
 
+import cyker.app.webservices.io.entity.UserEntity;
+import cyker.app.webservices.repository.UserRepository;
 import cyker.app.webservices.service.UserInterface;
 import cyker.app.webservices.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserInterface {
+
+    @Autowired
+    UserRepository userRepository;
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+
+        return returnValue;
     }
 }

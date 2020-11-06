1bebc0b2d5efe0aa372eb3b72feb4438e799ab35
Ciprian Carausu <carausuciprian@gmail.com>
2020-11-06 16:28:36 +0200

    UserRepository added

---
+++ src/main/java/cyker/app/webservices/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package cyker.app.webservices.repository;
+
+import cyker.app.webservices.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findUserByEmail(String email);
+}

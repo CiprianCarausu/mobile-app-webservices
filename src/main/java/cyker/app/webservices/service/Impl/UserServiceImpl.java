899d276e650a9b0431f03a97d30966afbe7b4ef0
Ciprian Carausu <carausuciprian@gmail.com>
2020-11-08 18:53:38 +0200

    Ecrypt user password

--- src/main/java/cyker/app/webservices/service/Impl/UserServiceImpl.java
+++ src/main/java/cyker/app/webservices/service/Impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import cyker.app.webservices.service.UserService;
 import cyker.app.webservices.shared.Utils;
 import cyker.app.webservices.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,16 +14,19 @@ public class UserServiceImpl implements UserService {
 
     UserRepository userRepository;
     Utils utils;
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, Utils utils) {
+    public UserServiceImpl(UserRepository userRepository, Utils utils, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.utils = utils;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public UserDto createUser(UserDto user) {
 
-        if(userRepository.findUserByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
+        if (userRepository.findUserByEmail(user.getEmail()) != null)
+            throw new RuntimeException("Record already exists");
 
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
@@ -30,7 +34,7 @@ public class UserServiceImpl implements UserService {
         String publicUserId = utils.generateUserId(30);
 
         userEntity.setUserId(publicUserId);
-        userEntity.setEncryptedPassword("test");
+        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
